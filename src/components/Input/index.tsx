4dbb7ed821fd5fa3e5722a025b7ae4149eaa4a48
davy <davydesouzabar@gmail.com>
2022-10-12 15:19:08 -0400

    feat: create input component to design system

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,31 @@
+import { Slot } from "@radix-ui/react-slot";
+import { IInput, ITextInputIcon, ITextInputRoot } from "./types";
+
+function TextInputRoot({ children }: ITextInputRoot) {
+  return (
+    <div className="flex  items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+      {children}
+    </div>
+  );
+}
+
+function TextInputIcon({ children }: ITextInputIcon) {
+  return <Slot className="w-6 h-6 text-gray-400">{children}</Slot>;
+}
+
+function Input(props: IInput) {
+  return (
+    <input
+      className={
+        "bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      }
+      {...props}
+    />
+  );
+}
+
+export const TextInput = {
+  TextInputRoot,
+  TextInputIcon,
+  Input,
+};
